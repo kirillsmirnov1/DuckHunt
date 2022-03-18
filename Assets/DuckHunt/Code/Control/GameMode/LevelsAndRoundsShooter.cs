c4ca987d13242702240a56f727c16446686eb50c
Kirill Smirnov <thegaldren@gmail.com>
2022-03-18 21:07:01 +0400

    Shooter mode sets cam rect

--- Assets/DuckHunt/Code/Control/GameMode/LevelsAndRoundsShooter.cs
+++ Assets/DuckHunt/Code/Control/GameMode/LevelsAndRoundsShooter.cs
@@ -14,6 +14,7 @@ namespace DuckHunt.Control.GameMode
         [SerializeField] private int targetsPerRound = 1;
         
         [SerializeField] private ATarget targetPrefab;
+        [SerializeField] private Rect camRect = new Rect(0, .2f, 1, .8f);
         
         private ShooterView _view;
         
@@ -21,6 +22,7 @@ namespace DuckHunt.Control.GameMode
         {
             _view = Instantiate(modeCanvas).GetComponent<ShooterView>();
             _view.Init(this);
+            Camera.main.rect = camRect;
         }
         
         // TODO Level Start / End 
