435daaf6a0ebfb02f8ef1b65da2218d1efc6e1b3
Kirill Smirnov <thegaldren@gmail.com>
2022-03-18 18:32:43 +0400

    Create AGameMode.cs and LevelsAndRoundsShooter.cs

---
+++ Assets/DuckHunt/Code/Control/GameMode/AGameMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DuckHunt.Control.GameMode
+{
+    public abstract class AGameMode : ScriptableObject
+    {
+        [SerializeField] protected string flowName;
+        
+        public abstract void Start();
+        public virtual bool ReadyToDisplay => !string.IsNullOrWhiteSpace(flowName);
+        public abstract bool ReadyToPlay { get; }
+    }
+}
---
+++ Assets/DuckHunt/Code/Control/GameMode/LevelsAndRoundsShooter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DuckHunt.Control.GameMode
+{
+    [CreateAssetMenu(menuName = "Modes/LevelsAndRoundsShooter", fileName = "LevelsAndRoundsShooter", order = 0)]
+    public class LevelsAndRoundsShooter : AGameMode
+    {
+        [SerializeField] private int numberOfLevels = 10;
+        [SerializeField] private int roundsPerLevel = 5;
+        [SerializeField] private int bulletsPerRound = 3;
+        [SerializeField] private int targetsPerRound = 1;
+        
+        [SerializeField] private GameObject targetPrefab;
+
+        public override void Start()
+        {
+            // TODO 
+        }
+
+        public override bool ReadyToPlay 
+            => targetPrefab != null;
+    }
+}
