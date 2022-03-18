e0b7109c266cfda85ed87e77197344f12df14646
Kirill Smirnov <thegaldren@gmail.com>
2022-03-18 19:20:36 +0400

    GameModesArray.cs

---
+++ Assets/DuckHunt/Code/Model/GameModesArray.cs
@@ -0,0 +1,22 @@
+﻿using DuckHunt.Control.GameMode;
+using DuckHunt.Utils;
+using MyBox;
+using UnityEditor;
+using UnityEngine;
+using UnityUtils.Variables;
+
+namespace DuckHunt.Model
+{
+    [CreateAssetMenu(menuName = "Modes/GameModesArray", fileName = "GameModesArray", order = 0)]
+    public class GameModesArray : XArrayVariable<AGameMode>
+    {
+#if UNITY_EDITOR
+        [ButtonMethod]
+        private void GetAllModes()
+        {
+            Value = SOUtils.GetAllInstances<AGameMode>();
+            EditorUtility.SetDirty(this);
+        }
+#endif
+    }
+}
