93d266ad3a381988561416a2cf10683586198ff2
Kirill Smirnov <thegaldren@gmail.com>
2022-03-18 19:20:17 +0400

    SOUtils.cs

---
+++ Assets/DuckHunt/Code/Utils/SOUtils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DuckHunt.Utils
+{
+    public static class SOUtils // TODO move to UU
+    {
+        public static T[] GetAllInstances<T>() where T : ScriptableObject
+        {
+            string[] guids = AssetDatabase.FindAssets("t:"+ typeof(T).Name);  //FindAssets uses tags check documentation for more info
+            T[] a = new T[guids.Length];
+            for(int i =0;i<guids.Length;i++)         //probably could get optimized 
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+            }
+ 
+            return a;
+ 
+        }
+        
+    }
+}
